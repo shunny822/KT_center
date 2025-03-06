7bc37bd106982b634334a9fb9c006cdaa328373f
shunny822 <sw92522@naver.com>
2025-03-06 17:12:19 +0900

    commit

--- lossreport/src/main/java/untitled/infra/LossReportController.java
+++ lossreport/src/main/java/untitled/infra/LossReportController.java
@@ -1,14 +1,15 @@
 package untitled.infra;
+import untitled.domain.*;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import untitled.domain.*;
 
 //<<< Clean Arch / Inbound Adaptor
 
@@ -16,8 +17,19 @@ import untitled.domain.*;
 // @RequestMapping(value="/lossReports")
 @Transactional
 public class LossReportController {
-
     @Autowired
     LossReportRepository lossReportRepository;
+
+    @RequestMapping(value = "/lossReports/reportloss",
+            method = RequestMethod.POST,
+            produces = "application/json;charset=UTF-8")
+    public LossReport reportLoss(HttpServletRequest request, HttpServletResponse response, 
+        ) throws Exception {
+            System.out.println("##### /lossReport/reportLoss  called #####");
+            LossReport lossReport = new LossReport();
+            lossReport.reportLoss();
+            lossReportRepository.save(lossReport);
+            return lossReport;
+    }
 }
 //>>> Clean Arch / Inbound Adaptor
